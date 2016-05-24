dabb4e4366e558d7b9e13014f73c4d9446e16ff8
Wang Qun Feng <qunfengw@gmail.com>
2016-05-24 15:54:07 +0800

    make game tests pass

--- lib/game.rb
+++ lib/game.rb
@@ -5,5 +5,26 @@ class Game
 	def initialize(tribe_a, tribe_b)
 		@tribe_a = tribe_a
 		@tribe_b = tribe_b
+		@tribes = [@tribe_a, @tribe_b]
+	end
+
+	def add_tribe(tribe)
+		@tribes << tribe
+	end
+
+	def clear_tribes
+		@tribes.clear
+	end
+
+	def immunity_challenge
+		@tribes.sample
+	end
+
+	def individual_immunity_challenge
+		@tribes.sample.members.sample
+	end
+
+	def merge(tribe_name)
+		Tribe.new(:name => tribe_name, :members => @tribes.map { |e| e.members  }.flatten)
 	end
 end
